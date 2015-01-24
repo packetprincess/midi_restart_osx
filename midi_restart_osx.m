20bbcfdf263ab35939ac9100b144d7d8bae517a6
No Stranger <stranger@Nos-MacBook-Pro.local>
2015-01-24 15:51:59 -0800

    first commit

---
+++ midi_restart_osx.m
@@ -0,0 +1,23 @@
+/* 
+Script to rescan midi devices
+compile with:
+gcc midi_restart_osx.m -o midi_restart_osx -ObjC -framework Foundation -framework CoreMIDI
+
+*/
+
+#import <CoreMIDI/CoreMIDI.h>
+#import <Foundation/Foundation.h>
+
+int main()
+{
+  int i=0;
+  while ( i==0 ){
+		     
+  OSStatus MIDIRestart ( void );
+  sleep(15);
+  
+  }
+    
+  return 0;
+  
+}
