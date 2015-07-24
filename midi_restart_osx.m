a30a83f4f7034de09ab703af1f32a6c33a3591ed
Jamie Schwettmann <jamie.schwettmann@gmail.com>
2015-07-23 18:16:30 -0700

    Updated control script
    Updated control script

--- midi_restart_osx.m
+++ midi_restart_osx.m
@@ -1,4 +1,4 @@
-/*********************************************
+/**************************************************************************
 Program to rescan midi devices
 @complexbits
 
@@ -6,7 +6,7 @@ compile with:
 
 gcc midi_restart_osx.m -o midi_restart_osx -ObjC -framework Foundation -framework CoreMIDI
 
-**********************************************/
+***************************************************************************/
 
 #import <CoreMIDI/CoreMIDI.h>
 #import <Foundation/Foundation.h>
@@ -21,8 +21,12 @@ int main()
   //  int i=0;
   //  while ( i==0 ){
 
-  OSStatus MIDIRestart ( void );
-  printf("success.\n");
+  OSStatus result=MIDIRestart();
+  if ( result==0 ){
+    printf("success!\n");
+  }else{
+    printf("error: %d.\n", result);
+  }
   
   // }
     
